8b84e702615578a7cb750c0b285281c30f0de4ef
Victor Wåhlström <victor.wahlstrom@initiali.se>
2016-12-12 21:34:16 +0100

    Add support for compiling against static Qt

--- ZtWidgets/include/ZtWidgets/ztwidgets_global.h
+++ ZtWidgets/include/ZtWidgets/ztwidgets_global.h
@@ -25,10 +25,14 @@
 
 #include <QtCore/QtGlobal>
 
-#if defined ZTWIDGETS_LIB
-#define ZTWIDGETS_EXPORT Q_DECL_EXPORT
+#ifndef QT_STATIC
+    #if defined ZTWIDGETS_LIB
+        #define ZTWIDGETS_EXPORT Q_DECL_EXPORT
+    #else
+        #define ZTWIDGETS_EXPORT Q_DECL_IMPORT
+    #endif
 #else
-#define ZTWIDGETS_EXPORT Q_DECL_IMPORT
+    #define ZTWIDGETS_EXPORT
 #endif
 
 #endif // ZTWIDGETS_GLOBAL_H
